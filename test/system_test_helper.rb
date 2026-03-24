835fe1859b6bd1bc74342b01008938c1ebc9b5ef
Anthony Georges <anthony@anthonygeorges.com>
2026-03-24 11:55:46 +1100

    attempt to fix system tests

--- test/system_test_helper.rb
+++ test/system_test_helper.rb
@@ -9,7 +9,19 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, headless: true, timeout: 15)
+  browser_options = {}
+  if ENV["CI"]
+    browser_options['no-sandbox'] = nil
+    browser_options['disable-dev-shm-usage'] = nil
+    browser_options['disable-gpu'] = nil
+  end
+  Capybara::Cuprite::Driver.new(
+    app,
+    headless: true,
+    timeout: 15,
+    process_timeout: 30,
+    browser_options: browser_options
+  )
 end
 Capybara.default_driver       = :cuprite
 Capybara.javascript_driver    = :cuprite
