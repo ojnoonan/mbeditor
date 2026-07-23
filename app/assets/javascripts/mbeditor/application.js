ad3b1ac29b2bc172f40e22a31cb0e09b14795a50
ojnoonan <ojnoonan@gmail.com>
2026-07-23 11:15:55 +1000

    feat: FileImport module for walking dropped directories

--- app/assets/javascripts/mbeditor/application.js
+++ app/assets/javascripts/mbeditor/application.js
@@ -2,6 +2,7 @@
 //= require mbeditor/editor_store
 //= require mbeditor/file_icon
 //= require mbeditor/file_service
+//= require mbeditor/file_import
 //= require mbeditor/history_service
 //= require mbeditor/websocket_service
 //= require mbeditor/git_service
